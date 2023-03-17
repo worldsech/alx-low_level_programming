d3ace5c7040233270f27ffb7ddc3f59447c2bf95
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-17 16:57:37 -0700

    0-positive_or_negative.c update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,28 +2,28 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *main - Entry point
- *Disciption: 'Check for number if it is positive or negative'
- *Return:  always 0
- */
+*main - Entry point
+*Disciption: 'Check for number if it is positive or negative'
+*Return:  always 0
+*/
 int main(void)
 {
-        int n;
+int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n>0)
-        {
-                printf("%d is position\n", n);
-        }
-        else if (n==0)
-        {
-                printf("%d is zero\n", n);
-        }
-        else
-        {
-                printf("%d is negative\n", n);
-	}
-        return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is position\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
 
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+*main - Entry point
+*Description: 'get thelast digit of a number'
+*Return: always 0
+*/
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n>0)
-	{
-		printf("%d is position\n", n);
-	}
-	else if (n==0)
-	{
-		printf("%d is zero\n", n)
-	}
-	else
-	{
-		printf("%d is negative\n", n)
-	)
-	return (0);
+int n;
+int ldigit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+ldigit = n % 10;
+if (ldigit > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, ldigit);
+}
+else if (ldigit == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, ldigit);
+}
+else
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ldigit);
+}
+return (0);
 }
