18744d7276327611fe03f1455ecca7614fa2c497
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-17 19:22:53 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: '.'
+*Return: always 0alphabet in reverse
+*/
+int main(void)
+{
+int n = 122;
+while (n > 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
