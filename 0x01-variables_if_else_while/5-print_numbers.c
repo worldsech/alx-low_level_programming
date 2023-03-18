32f76c22c43b604a983a4a0272fdd13e720dc231
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-17 18:52:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'numbers of base 10 starting from 0, followed by a new line.'
+*Return: always 0
+*/
+int main(void)
+{
+
+for (int n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
