6ddcfff7ce4a30cdb69098ceed2d40d46e691ed4
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-17 17:36:49 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'displaying lowercase alphabet''
+*Return: always 0
+*/
+int main(void)
+{
+for(int n = 97; n <= 122; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
