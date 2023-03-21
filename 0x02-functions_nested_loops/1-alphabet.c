ff12d4e3d4e5e8b4cd7b301b27362e84707f9779
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-21 08:58:46 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - check the code
+*Description: 'print alphabet function'
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+int i = 97;
+while (i <= 122)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
