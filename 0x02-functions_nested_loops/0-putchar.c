0d66626080413ba344be033054b81418d9eabd20
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-21 06:44:25 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'combinations of single-digit numbers.'
+*Return: always 0
+*/
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
