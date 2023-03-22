563cb965304c3a79ec756034d66d1f1b5ddea530
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-22 11:07:10 -0700

    betty update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,27 +8,27 @@
 void print_to_98(int n)
 {
 int i;
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
-	{
-            printf("%d", i);
-            if (i != 98)
-	    {
-                printf(", ");
-            }
-        }
-    }
-    else
-    {
-        for (i = n; i >= 98; i--)
-	{
-            printf("%d", i);
-            if (i != 98)
-	    {
-                printf(", ");
-            }
-        }
-    }
-    printf("\n");
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+printf("\n");
 }
