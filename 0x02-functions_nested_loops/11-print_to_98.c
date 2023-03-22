796546e635409c78086d8d02c7899354838b1de9
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-22 10:59:44 -0700

    98 update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,38 +4,31 @@
  * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line
  * @n: print from this number
- */
+*/
 void print_to_98(int n)
 {
-int i, j;
-if (n <= 98)
-{
-	for (i = n; i <= 98; i++)
+int i;
+    if (n <= 98)
+    {
+        for (i = n; i <= 98; i++)
 	{
-		if (i != 98)
-		{
-			_putchar(i);
-			_putchar(',');
-		}
-		else if (i == 98)
-		{
-			_putchar(i);
-		}
-	}
-}
-else if (n >= 98)
-{
-	for (j = n; j >= 98; j++)
+            printf("%d", i);
+            if (i != 98)
+	    {
+                printf(", ");
+            }
+        }
+    }
+    else
+    {
+        for (i = n; i >= 98; i--)
 	{
-		if (j != 98)
-		{
-			_putchar(j);
-			_putchar(',');
-		}
-		else if (j == 98)
-		{
-			_putchar(j);
-		}
-	}
-}
+            printf("%d", i);
+            if (i != 98)
+	    {
+                printf(", ");
+            }
+        }
+    }
+    printf("\n");
 }
