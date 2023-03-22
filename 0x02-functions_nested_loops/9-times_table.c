16318631283d3521d49c232c89a51289f77cb1c5
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-22 05:54:01 -0700

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 int i, j;
 for (i = 0; i <= 9; i++)
 {
-for (j = 1; j <= 9; j++)
+for (j = 0; j <= 9; j++)
 {
 int result = i * j;
 if (result >= 10)
@@ -19,8 +19,10 @@ else
 _putchar(' ');
 }
 _putchar(result % 10 + '0');
+if (j != 9){
 _putchar(',');
 _putchar(' ');
+}
 }
 _putchar('\n');
 }
