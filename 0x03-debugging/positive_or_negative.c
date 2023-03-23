b7c47ef92950e325f6184d25ddf4e836ddd27a13
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-23 03:02:51 -0700

    positive or nagative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * positive_or_negative: to test fot zero
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+	printf("%d is zero\n", i);
+	}
+	else
+	{
+	printf("%d is negative\n", i);
+	}
+}
