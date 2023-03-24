b45c812bba0810a4d5e4ead2317b633ca2e824bc
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-24 06:50:26 -0700

    update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,6 +7,7 @@
 
 void print_diagonal(int n)
 {
+
 	if (n <= 0)
 	{
 		_putchar ('\n');
@@ -15,9 +16,9 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j <= i; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar (' ');
 			}
