73bd454a3aae371bd77c409d1293beec10d550f3
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-24 01:07:23 -0700

    betty update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _islower - check if char is lowercase
+ * _isupper - check if char is uppercase
  * @c: is the char to be checked
- * Return: 1 if char is lowercase, otherwise 0.
+ * Return: 1 if char is uppercase, otherwise 0.
  */
 int _isupper(int c)
 {
