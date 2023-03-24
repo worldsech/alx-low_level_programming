9855b17543f5f65d98c696c84a196a8317b0ae7b
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-24 02:41:27 -0700

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers- print numbers from 1 -14 0 time
+ *
+ * return - 0
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
