c308d4d10b98e028b1071b33b50f70d4ee820ae8
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-24 08:03:20 -0700

    updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,25 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf ("FizzBuzz ");
+			printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf ("Fizz ");
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf ("Buzz ");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf ("%d ", i);
+			printf("%d ", i);
 		}
 	}
-	printf ("\n");
+	printf("\n");
 	return (0);
 }
