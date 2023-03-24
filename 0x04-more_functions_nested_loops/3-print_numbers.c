de748e3bd8f2626cfa4adfcfbc2e62430b9b0e85
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-03-24 01:58:23 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar (i);
+	}
+	_putchar ('\n');
+}
