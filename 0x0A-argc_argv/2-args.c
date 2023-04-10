e33910a907ba95587a45c426faaea4bcbe0d6383
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-04-10 13:15:21 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+	for (count = 0; count < argc; count++)
+	{
+		printf ("%s\n", argv[count]);
+	}
+	return (0);
+}	
