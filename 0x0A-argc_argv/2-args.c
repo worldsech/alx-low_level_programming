0f1e7c0151290074e33074152402958fd7f5f112
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-04-10 13:42:16 -0700

    3-mul.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,9 +11,10 @@
 int main(int argc, char *argv[])
 {
 	int count;
+
 	for (count = 0; count < argc; count++)
 	{
-		printf ("%s\n", argv[count]);
+		printf("%s\n", argv[count]);
 	}
 	return (0);
-}	
+}
